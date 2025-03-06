1defe841f046eb2959dc78d050996d8d4034e9cd
Cleudes da Silva <cleudesprogramador80@gmail.com>
2025-03-06 20:13:46 -0300

    Made test insert in class Program2

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -23,10 +23,11 @@ public class Program2 {
 			System.out.println(obj);
 		}
 		
-
+		System.out.println("=== TEST 3: department insert ===");
 		
-		
-		
+		Department newDepartment = new Department(null, "Music");
+		depDao.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
 		
 	}
 
